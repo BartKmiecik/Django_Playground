d5d21040eb20bd16029009d546261b88f7447510
Bart <kmiecikjob@gmail.com>
2025-03-21 20:02:40 +0100

    Playing with diffrent responsesUse api viewUse serialization model_to_dict

--- py_client/basic.py
+++ py_client/basic.py
@@ -6,6 +6,7 @@ endpoint2 = 'http://www.httpbin.org/status/200'
 myEndpoint = 'http://127.0.0.1:8000/api/'
 
 get_request = requests.get(myEndpoint, json={"product_id": 123})
-print(get_request.text)
+# print(get_request.headers)
+# print(get_request.text)
 print(get_request.json())
-print(get_request.status_code)
+# print(get_request.status_code)
