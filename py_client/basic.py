3abca9dc1e3a1414e6ef806533d247ec77ea1987
Bart <kmiecikjob@gmail.com>
2025-03-26 20:21:04 +0100

    Using serializer to post and verify stuff

--- py_client/basic.py
+++ py_client/basic.py
@@ -5,7 +5,7 @@ endpoint = 'http://www.httpbin.org/anything'
 endpoint2 = 'http://www.httpbin.org/status/200'
 myEndpoint = 'http://127.0.0.1:8000/api/'
 
-get_request = requests.get(myEndpoint, json={"product_id": 123})
+get_request = requests.post(myEndpoint, json={"title": "Hello post"})
 # print(get_request.headers)
 # print(get_request.text)
 print(get_request.json())
